c9c02b5423f7caaac7c86fb367e905008368a56d
jotahtsx <joaomanoelskai@gmail.com>
2022-07-28 10:09:11 -0300

    pauloCoelhoAdv || foreach faq

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -6,7 +6,8 @@
             <h2>Atualizações</h2>
             @if ($sliders->count())
                 @foreach ($sliders as $slider)
-                    <article class="swiper-slide" style="background: url({{ '' . '../..' . Storage::url($slider->cover) }})">
+                    <article class="swiper-slide"
+                        style="background: url({{ '~advogado' . '../..' . Storage::url($slider->cover) }})">
                         <div class="swiper-slide-inner">
                             <div>
                                 {!! $slider->content !!}
